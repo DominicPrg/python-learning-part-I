030a1bca2ae3525ea8947bc8243c507f8f97c2a5
Dom. <157545817+DominicPrg@users.noreply.github.com>
2025-06-20 12:13:34 -0700

    Create summing_a_million.py

---
+++ week_#2/summing_a_million.py
@@ -0,0 +1,10 @@
+list_of_million = [number for number in range(1,1_000_001)]
+
+#Print the element with the least value, numerically. '1' should be printed.
+print(min(list_of_million))
+
+#Print the element with the greatest value, numerically. '1_000_000' should be printed.
+print(max(list_of_million))
+
+#Print the sum of all the elements in the list.
+print(sum(list_of_million))
