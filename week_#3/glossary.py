ecd52e73d6c9aaaea7a53454bfe07d707abdb691
Dom. <157545817+DominicPrg@users.noreply.github.com>
2025-06-27 13:40:32 -0700

    Create glossary.py

---
+++ week_#3/glossary.py
@@ -0,0 +1,14 @@
+glossary = {
+    'immutable':'A value that cannot be changed.',
+    'tuple':'An immutable list',
+    'traceback':'A record of where the interpreter experienced trouble during'
+                ' execution of the code.',
+    'method':'An action that Python can perform on a piece of data.',
+    'f-string':'A string formatted to output a value of a variable.',
+}
+
+print(f"Immutable: {glossary['immutable']}\n")
+print(f"Tuple: {glossary['tuple']}\n")
+print(f"Traceback: {glossary['traceback']}\n")
+print(f"Method: {glossary['method']}\n")
+print(f"F-string: {glossary['f-string']}\n")
